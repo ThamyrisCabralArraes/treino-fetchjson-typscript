fdfedf134cd08095b2db0d033d73fb817ac14345
thamyris <thamyriscabral@hotmail.com>
2023-03-07 19:27:36 -0300

    feat: criado variaveis

--- types-ts/types.js
+++ types-ts/types.js
@@ -1,1 +1,19 @@
-console.log('first2');
+var quantidade = 5; //nesse caso melhor usar const quantidade = 5
+var umFilme = 'Tubarão'; //nesse caso melhor usar const umFilme = 5
+var oFilmeEhBom = true; //nesse caso melhor usar const oFilmeEhBom = 5
+var nothingMuch = null; //nesse caso melhor usar const nothingMuch = 5
+var nothingUndefined = undefined; //nesse caso melhor usar const nothingUndefined = 5
+var now = new Date();
+//Array
+var colors = ['vermelho', 'verde', 'azul'];
+var meuNumeros = [1, 3, 2];
+var truths = [true, false, false];
+// Object
+var point = {
+    x: 1,
+    y: 2,
+    z: 3,
+    person1: 'thamyris'
+};
+//function
+var exemploFunction = function (i) { };
--- types-ts/types.ts
+++ types-ts/types.ts
@@ -1,1 +1,32 @@
-console.log('first2');
+const quantidade: number = 5; //nesse caso melhor usar const quantidade = 5
+const umFilme: string = 'Tubarão'; //nesse caso melhor usar const umFilme = 5
+const oFilmeEhBom: boolean = true; //nesse caso melhor usar const oFilmeEhBom = 5
+
+const nothingMuch: null = null; //nesse caso melhor usar const nothingMuch = 5
+const nothingUndefined: undefined = undefined; //nesse caso melhor usar const nothingUndefined = 5
+
+const now: Date = new Date();
+
+//Array
+const colors: string[] = ['vermelho', 'verde', 'azul'];
+
+const meuNumeros: number[] = [1, 3, 2];
+
+const truths: boolean[] = [true, false, false];
+
+// Object
+const point: {
+  x: number;
+  y: number;
+  z: number;
+  person1: string;
+  person2?: string;
+} = {
+  x: 1,
+  y: 2,
+  z: 3,
+  person1: 'thamyris',
+};
+
+//function
+const exemploFunction: (i: number) => void = (i) => {};
