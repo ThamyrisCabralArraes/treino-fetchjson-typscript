9d93bb5c87112fa0830e6ec4fc857a0bfde2a350
thamyris <thamyriscabral@hotmail.com>
2023-03-08 11:40:22 -0300

    feat: exemplos array

--- types-ts/array.js
+++ types-ts/array.js
@@ -1,1 +1,19 @@
-console.log('first');
+var array1 = ['aaa', 'bbbb'];
+var array2 = [new Date(), new Date()];
+// array de arrays
+var array3 = [['aaa'], ['bbb']];
+// array com objeto
+var array4 = [{ a: 1, b: 2 }];
+// map
+array1.map(function (item) {
+    return item.length;
+});
+var array5 = ['idade', 30];
+var mapDaArray5 = array5.map(function (item) {
+    return "Resultado ".concat(item);
+});
+//tuple
+var array6 = ['aaa', 10, true];
+var array7 = ['aaa', 1, true];
+var array8 = ['bbb', 2, false];
+var array9 = ['ccc', 3, true];
--- types-ts/array.ts
+++ types-ts/array.ts
@@ -1,1 +1,32 @@
-console.log('first');
+const array1: string[] = ['aaa', 'bbbb'];
+const array2: Date[] = [new Date(), new Date()];
+
+// array de arrays
+const array3: string[][] = [['aaa'], ['bbb']];
+
+// array com objeto
+const array4: { a: number; b: number }[] = [{ a: 1, b: 2 }];
+
+// map
+
+array1.map((item: string): number => {
+  return item.length;
+});
+
+const array5: (string | number)[] = ['idade', 30];
+
+const mapDaArray5 = array5.map((item: string | number): string => {
+  return `Resultado ${item}`;
+});
+
+//tuple
+
+const array6: [string | number, number, boolean] = ['aaa', 10, true];
+
+//type alias
+
+type Exemplo = [string | number, number, boolean];
+
+const array7: Exemplo = ['aaa', 1, true];
+const array8: Exemplo = ['bbb', 2, false];
+const array9: Exemplo = ['ccc', 3, true];
