355c6b822d94576eb74cfa38fca4ae76e32d36e9
thamyris <thamyriscabral@hotmail.com>
2023-03-08 10:56:14 -0300

    feat: crei a pasta para exemplos de array

--- types-ts/objects.ts
+++ types-ts/objects.ts
@@ -1,1 +1,16 @@
-console.log('first');
+const profile = {
+  nome: 'thamy',
+  age: 34,
+  coord: {
+    lat: 0,
+    lng: 15,
+  },
+  setAge(age: number) {
+    this.age = age;
+  },
+};
+
+const { age, nome }: { age: number; nome: string } = profile;
+const {
+  coord: { lat, lng },
+}: { coord: { lat: number; lng: number } } = profile;
