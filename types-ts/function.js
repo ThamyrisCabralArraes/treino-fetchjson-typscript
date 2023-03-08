d69c18e0891047cdc57d4ae3b5f557c310a328c2
thamyris <thamyriscabral@hotmail.com>
2023-03-08 10:31:25 -0300

    feat; crei a pasta para exemplos de objects

--- types-ts/function.js
+++ types-ts/function.js
@@ -1,1 +1,38 @@
-console.log('first');
+//o number da funçao nao precisa ser descrito nesse caso mas sempre deve ser colocado
+// se vc nao colocar e esquecer o retorno ela fca void
+var add = function (x, y) {
+    return x + y;
+};
+// outros tipos de funcao sem ser arrow
+function divide(a, b) {
+    return a / b;
+}
+var multiplicacao = function (a, b) {
+    return a * b;
+};
+// void
+var voids = function (message) {
+    message;
+};
+// never - dificil usar mas tem essa opcao, melhor usar void
+var throwErr = function (message) {
+    throw new Error(message);
+};
+// funcao usando destruturão
+var weatherToday = {
+    date: new Date(),
+    weather: 'sunny'
+};
+// aqui sem destructuring
+var showWeather = function (weathers) {
+    weathers.date;
+    weathers.weather;
+};
+showWeather(weatherToday);
+// aqui com destructuring
+var showWeatherComDestruturacao = function (_a) {
+    var date = _a.date, weather = _a.weather;
+    date;
+    weather;
+};
+showWeatherComDestruturacao(weatherToday);
--- types-ts/function.ts
+++ types-ts/function.ts
@@ -1,1 +1,55 @@
-console.log('first');
+//o number da funçao nao precisa ser descrito nesse caso mas sempre deve ser colocado
+// se vc nao colocar e esquecer o retorno ela fca void
+const add = (x: number, y: number): number => {
+  return x + y;
+};
+
+// outros tipos de funcao sem ser arrow
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiplicacao = function (a: number, b: number): number {
+  return a * b;
+};
+
+// void
+
+const voids = (message): void => {
+  message;
+};
+
+// never - dificil usar mas tem essa opcao, melhor usar void
+
+const throwErr = (message: string): never => {
+  throw new Error(message);
+};
+
+// funcao usando destruturão
+
+const weatherToday = {
+  date: new Date(),
+  weather: 'sunny',
+};
+// aqui sem destructuring
+const showWeather = (weathers: { date: Date; weather: string }): void => {
+  weathers.date;
+  weathers.weather;
+};
+
+showWeather(weatherToday);
+
+// aqui com destructuring
+
+const showWeatherComDestruturacao = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  date;
+  weather;
+};
+
+showWeatherComDestruturacao(weatherToday);
